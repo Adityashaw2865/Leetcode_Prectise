4a4824e75d552ce5d155bd3f52fad0c67322b6f5
Aditya Kumar Shaw <aks09adi@gmail.com>
2026-06-27 22:42:30 +0530

    Time: 0 ms (100%), Space: 8.5 MB (65.41%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        string s1 = "", t1 = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '#') {
+                if (!s1.empty()) {
+                    s1.pop_back();
+                }
+            } else {
+                s1 += s[i];
+            }
+        }
+        for (int i = 0; i < t.length(); i++) {
+            if (t[i] == '#') {
+                if (!t1.empty()) {
+                    t1.pop_back();
+                }
+            } else {
+                t1 += t[i];
+            }
+        }
+        return s1 == t1;
+    }
+
+};
