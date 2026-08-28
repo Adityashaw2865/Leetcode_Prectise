1f104344c2589e11356446ab2c42cd14a306dae2
Aditya Kumar Shaw <aks09adi@gmail.com>
2026-08-28 20:01:39 +0530

    Time: 38 ms (25.5%), Space: 25.5 MB (7.9%) - LeetHub

--- 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.cpp
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     int maxDistinct(string s) {
-        int sz=0;
-        set<char>st(s.begin(),s.end());
-        for(char c :st){
-            sz++;
-        }
-        return sz;
+        set<char> st(s.begin(), s.end());
+        return st.size();
     }
 };
