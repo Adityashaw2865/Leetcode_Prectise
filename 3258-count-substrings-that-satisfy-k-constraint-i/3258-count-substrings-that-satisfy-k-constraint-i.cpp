1038a62af568f8dbbe045339967ac3c2f628406d
Aditya Kumar Shaw <aks09adi@gmail.com>
2026-09-20 01:22:28 +0530

    Time: 0 ms (100%), Space: 8.9 MB (64.82%) - LeetHub

---
+++ 3258-count-substrings-that-satisfy-k-constraint-i/3258-count-substrings-that-satisfy-k-constraint-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countKConstraintSubstrings(string s, int k) {
+        int n = s.size();
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            int count0 = 0;
+            int count1 = 0;
+            for (int j = i; j < n; j++) {
+                if (s[j] == '0') {
+                    count0++;
+                } else {
+                    count1++;
+                }
+                if (count0 <= k || count1 <= k) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
